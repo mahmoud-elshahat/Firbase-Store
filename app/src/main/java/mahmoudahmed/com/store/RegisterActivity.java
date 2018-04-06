8d66c07a83d30708043cb783ec6b004d07e1223d
Mahmoud Ahmed <mahmoudahmedsm@gmail.com>
2018-04-06 08:21:06 +0200

    Authentication user interface .

--- app/src/main/java/mahmoudahmed/com/store/RegisterActivity.java
+++ app/src/main/java/mahmoudahmed/com/store/RegisterActivity.java
@@ -5,24 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
 
 
-    public void Login(View view) {
 
 
+    public void openLogin(View view) {
+        Intent login =new Intent (this , MainActivity.class);
+        startActivity(login);
     }
 
-    public void openRegister(View view) {
-        Intent register =new Intent (this , RegisterActivity.class);
-        startActivity(register);
-    }
+    public void Register(View view) {
 
+    }
 }
